134d2814b395d37affddf4a13e3e0a725574654f
Angelo Zero <angelo.akm@gmail.com>
2021-03-04 00:16:55 -0300

    configs if the application don't find the database setup

--- src/main/java/com/angelozero/gibao/Application.java
+++ src/main/java/com/angelozero/gibao/Application.java
@@ -1,13 +1,18 @@
 package com.angelozero.gibao;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+/* Uncomment this line if you don't have the database configuration setup*/
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
